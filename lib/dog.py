f994a96b423780390b657e73bc7cddc33da5e497
Elchonon <ekn400@gmail.com>
2024-12-11 15:56:53 -0500

    lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,30 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name="Bark",breed="Mastiff"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self,new_name):
+        if type(new_name) == str and len(new_name) > 1 and len(new_name) < 25:
+            self._name = new_name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self,new_breed):
+        if APPROVED_BREEDS.count(new_breed) > 0:
+            self._breed = new_breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    
+    name = property(get_name,set_name)
+    breed = property(get_breed,set_breed)
+
+fido = Dog("namehere","Mastiff")
+print(fido)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,31 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Someone",job="Admin"):
+        self.name = name
+        self.job = job
+    
+    def get_name(self):
+        return self._name
+
+    def set_name(self,new_name):
+        if type(new_name) == str and len(new_name) > 1 and len(new_name) < 25:
+            self._name = new_name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_job(self):
+        return self._job
+
+    def set_job(self,new_job):
+        approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+        if approved_jobs.count(new_job) > 0:
+            self._job = new_job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
+someone = Person()
+print(someone.name)
